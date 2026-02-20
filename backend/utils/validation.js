39117787393262655239e1511f3cfcaf3dabaeb0
Mateen12a <ajidagbamateen12@gmail.com>
2026-02-20 18:01:40 +0100

    GHW26.86

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -330,6 +330,10 @@ function validateTaskCreation(data) {
   const descriptionResult = validateTaskDescription(data.description);
   if (!descriptionResult.valid) errors.push(descriptionResult.error);
 
+  if (!data.location) {
+    errors.push('Location is required');
+  }
+
   if (!data.duration) {
     errors.push('Duration is required');
   }
